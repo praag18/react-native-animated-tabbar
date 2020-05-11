5e156e8db9cccbe46f605a89c9306fc5050430e1
praag18 <34586357+praag18@users.noreply.github.com>
2020-05-11 09:28:50 +0530

    Update BubbleTabBar.tsx

--- src/bubble/BubbleTabBar.tsx
+++ src/bubble/BubbleTabBar.tsx
@@ -1,74 +1,218 @@
 import React, { useMemo, memo } from 'react';
-import { View, ViewStyle, StyleProp } from 'react-native';
-import { useSafeArea } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { State, TapGestureHandler } from 'react-native-gesture-handler';
+import {
+  interpolateColor,
+  onGestureEvent,
+  useValue,
+} from 'react-native-redash';
 // @ts-ignore 😞
 import isEqual from 'lodash.isequal';
-import BubbleTabBarItem from './item';
+import { withTransition } from '../../withTransition';
 import {
-  DEFAULT_ITEM_ANIMATION_DURATION,
-  DEFAULT_ITEM_ANIMATION_EASING,
   DEFAULT_ITEM_INNER_SPACE,
   DEFAULT_ITEM_OUTER_SPACE,
-  DEFAULT_ITEM_ICON_SIZE,
-  DEFAULT_ITEM_LAYOUT_DIRECTION,
-} from './constants';
-import { TabBarViewProps } from '../types';
-import { BubbleTabConfig } from './types';
+} from '../constants';
+import { TabBarItemProps } from '../../types';
+import { BubbleTabConfig } from '../types';
 import { styles } from './styles';
 
-const BubbleTabBarComponent = ({
-  selectedIndex,
-  routes,
-  duration = DEFAULT_ITEM_ANIMATION_DURATION,
-  easing = DEFAULT_ITEM_ANIMATION_EASING,
-  itemInnerSpace = DEFAULT_ITEM_INNER_SPACE,
-  itemOuterSpace = DEFAULT_ITEM_OUTER_SPACE,
-  iconSize = DEFAULT_ITEM_ICON_SIZE,
-  isRTL = DEFAULT_ITEM_LAYOUT_DIRECTION,
-  style: containerStyleOverride,
-}: TabBarViewProps<BubbleTabConfig>) => {
-  //#region Hooks
-  const safeArea = useSafeArea();
+const { add, interpolate, useCode, set, cond, onChange, eq } = Animated;
+
+const gestureHandler = (state: Animated.Value<State>) =>
+  onGestureEvent({ state });
+
+export type BubbleTabBarItemProps = TabBarItemProps & BubbleTabConfig;
+
+const BubbleTabBarItemComponent = (props: BubbleTabBarItemProps) => {
+  // props
+  const {
+    index,
+    selectedIndex,
+    label,
+    icon,
+    background,
+    labelStyle: labelStyleOverride,
+    duration,
+    easing,
+    itemInnerSpace,
+    itemOuterSpace,
+    iconSize,
+    isRTL,
+  } = props;
+
+  // variables
+  const {
+    itemInnerVerticalSpace,
+    itemInnerHorizontalSpace,
+    itemOuterVerticalSpace,
+    itemOuterHorizontalSpace,
+  } = useMemo(() => {
+    let _itemInnerVerticalSpace,
+      _itemInnerHorizontalSpace,
+      _itemOuterVerticalSpace,
+      _itemOuterHorizontalSpace = 0;
+
+    if (typeof itemInnerSpace === 'number') {
+      _itemInnerVerticalSpace = itemInnerSpace;
+      _itemInnerHorizontalSpace = itemInnerSpace;
+    } else {
+      _itemInnerVerticalSpace =
+        itemInnerSpace?.vertical || DEFAULT_ITEM_INNER_SPACE;
+      _itemInnerHorizontalSpace =
+        itemInnerSpace?.horizontal || DEFAULT_ITEM_INNER_SPACE;
+    }
+    if (typeof itemOuterSpace === 'number') {
+      _itemOuterVerticalSpace = itemOuterSpace;
+      _itemOuterHorizontalSpace = itemOuterSpace;
+    } else {
+      _itemOuterVerticalSpace =
+        itemOuterSpace?.vertical || DEFAULT_ITEM_OUTER_SPACE;
+      _itemOuterHorizontalSpace =
+        itemOuterSpace?.horizontal || DEFAULT_ITEM_OUTER_SPACE;
+    }
+    return {
+      itemInnerVerticalSpace: _itemInnerVerticalSpace,
+      itemInnerHorizontalSpace: _itemInnerHorizontalSpace,
+      itemOuterVerticalSpace: _itemOuterVerticalSpace,
+      itemOuterHorizontalSpace: _itemOuterHorizontalSpace,
+    };
+  }, [itemInnerSpace, itemOuterSpace]);
+  const labelWidth = useValue(0);
+
+  /**
+   * @DEV
+   * min width is calculated by adding outer & inner spaces
+   * with the icon size.
+   */
+  const minWidth = useMemo(() => {
+    return (
+      itemInnerHorizontalSpace * 2 + iconSize + itemOuterHorizontalSpace * 2
+    );
+  }, [itemInnerHorizontalSpace, itemOuterHorizontalSpace, iconSize]);
+  /**
+   * @DEV
+   * max width is calculated by adding inner space with label width
+   */
+  const maxWidth = add(labelWidth, itemInnerHorizontalSpace, minWidth);
+
+  // animations
+  const gestureState = useValue(State.UNDETERMINED);
+  const animatedFocus = withTransition({
+    index,
+    selectedIndex,
+    duration,
+    easing,
+  });
+  const animatedIconColor = interpolateColor(animatedFocus, {
+    inputRange: [0, 1],
+    outputRange: [icon.inactiveColor, icon.activeColor],
+  });
+
+  //#region styles
+  const containerStyle = [
+    styles.container,
+    {
+      paddingHorizontal: itemOuterHorizontalSpace,
+      paddingVertical: itemOuterVerticalSpace,
+      width: interpolate(animatedFocus, {
+        inputRange: [0, 1],
+        outputRange: [minWidth, maxWidth],
+      }),
+    },
+  ];
+  const contentContainerStyle = [
+    styles.contentContainer,
+    {
+      flexDirection: isRTL ? 'row-reverse' : 'row',
+      paddingHorizontal: itemInnerHorizontalSpace,
+      paddingVertical: itemInnerVerticalSpace,
+      borderRadius: itemInnerVerticalSpace * 2 + iconSize,
+      backgroundColor: interpolateColor(animatedFocus, {
+        inputRange: [0, 1],
+        outputRange: [background.inactiveColor, background.activeColor],
+      }),
+    },
+  ];
+  const labelContainerStyle = [
+    styles.labelContainer,
+    {
+      opacity: interpolate(animatedFocus, {
+        inputRange: [0.33, 1],
+        outputRange: [0, 1],
+      }),
+      [isRTL ? 'left' : 'right']: interpolate(animatedFocus, {
+        inputRange: [0, 1],
+        outputRange: [0, itemInnerHorizontalSpace + itemOuterHorizontalSpace],
+      }),
+    },
+  ];
+  const iconContainerStyle = [
+    styles.iconContainer,
+    {
+      minHeight: iconSize,
+      minWidth: iconSize,
+    },
+  ];
+  const labelStyle = [styles.label, labelStyleOverride];
   //#endregion
 
-  //#region Styles
-  const containerStyle = useMemo<StyleProp<ViewStyle>>(
+  // callbacks
+  const handleTextlayout = ({
+    nativeEvent: {
+      // @ts-ignore
+      layout: { width },
+    },
+  }) => requestAnimationFrame(() => labelWidth.setValue(width));
+
+  // effects
+  useCode(
     () => [
-      styles.container,
-      containerStyleOverride,
-      {
-        flexDirection: isRTL ? 'row-reverse' : 'row',
-        paddingBottom: safeArea.bottom,
-      },
+      onChange(
+        gestureState,
+        cond(eq(gestureState, State.END), set(selectedIndex, index))
+      ),
     ],
-    [safeArea, containerStyleOverride, isRTL]
+    [gestureState, index]
   );
-  //#endregion
+
+  // render
+  const renderIcon = () => {
+    return typeof icon.component === 'function'
+      ? icon.component({
+          animatedFocus,
+          color: animatedIconColor,
+          size: iconSize,
+        })
+      : icon.component;
+  };
+
   return (
-    <View style={containerStyle}>
-      {routes.map(({ key, title, ...configs }, index) => {
-        return (
-          <BubbleTabBarItem
-            key={key}
-            index={index}
-            selectedIndex={selectedIndex}
-            label={title}
-            duration={duration}
-            easing={easing}
-            itemInnerSpace={itemInnerSpace}
-            itemOuterSpace={itemOuterSpace}
-            iconSize={iconSize}
-            isRTL={isRTL}
-            {...configs}
-          />
-        );
-      })}
-    </View>
+      <View style={{flex: 1,justifyContent: 'center',alignItems: 'center'}}>
+        <TapGestureHandler {...gestureHandler(gestureState)}>
+          <Animated.View style={containerStyle}>
+            <Animated.View style={contentContainerStyle}>
+              <View style={iconContainerStyle}>{renderIcon()}</View>
+            </Animated.View>
+            <Animated.View style={labelContainerStyle}>
+              <Text
+                  onLayout={handleTextlayout}
+                  style={labelStyle}
+                  numberOfLines={1}
+              >
+                {label}
+              </Text>
+            </Animated.View>
+          </Animated.View>
+        </TapGestureHandler>
+      </View>
   );
 };
 
-const BubbleTabBar = memo(BubbleTabBarComponent, (prevProps, nextProps) =>
-  isEqual(prevProps, nextProps)
+const BubbleTabBarItem = memo(
+  BubbleTabBarItemComponent,
+  (prevProps, nextProps) => isEqual(prevProps, nextProps)
 );
 
-export default BubbleTabBar;
+export default BubbleTabBarItem;
